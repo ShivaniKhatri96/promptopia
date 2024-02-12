fbdf3741b9b0aee2ae6aa2cd8ed0b73c57fcb3ec
Shivani Khatri <khatri.shivani96@gmail.com>
2024-02-12 01:04:41 +0100

    GET prompt api created

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -7,10 +7,12 @@ export const GET = async (request, { params }) => {
     await connectToDB();
     const prompt = await Prompt.findById(params.id).populate('creator')
     if(!prompt) return new Response('Prompt not found', {status: 404});
-  } catch (error) {
-    return new Response(JSON.stringify(prompts), { status: 200 });
+   return new Response(JSON.stringify(prompt),{status: 200})
   }
-};
+  catch (error) {
+    return new Response('Failed to fetch all prompts', { status: 500 });
+  }
+}
 
 // PATCH (update)
 
