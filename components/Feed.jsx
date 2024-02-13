062b47c7b61af5f33b3b5e8d2e8b448d2abeb450
Shivani Khatri <khatri.shivani96@gmail.com>
2024-02-14 00:16:30 +0100

    searchbar implemented using URL search params

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,50 +1,62 @@
 "use client";
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post) => (
-        <PromptCard 
-        key={post._id}
-        post={post}
-        handleTagClick={handleTagClick}
+      {data?.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
         />
       ))}
     </div>
-  )
-}
+  );
+};
 const Feed = () => {
-  const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([])
-  const handleSearchChange = (e) => {
-
-  }
+  const [posts, setPosts] = useState([]);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearchChange = (term) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
+      const response = await fetch("/api/prompt");
       const data = await response.json();
-      setPosts(data)
-    }
-    fetchPosts()
-  }, [])
-  
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+  const query = searchParams.get("query")?.toString() || "";
+  const filteredPosts = posts.filter(
+    (post) =>
+      query === "" ||
+      post.tag.toLowerCase().includes(query.toLowerCase()) ||
+      post.prompt.toLowerCase().includes(query.toLowerCase())
+  );
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
           type="text"
           placeholder="Search for a tag or a username"
-          value={searchText}
-          onChange={handleSearchChange}
+          onChange={(e) => handleSearchChange(e.target.value)}
           required
           className="search_input peer"
+          defaultValue={query}
         />
       </form>
-      <PromptCardList 
-        data={posts}
-        handleTagClick={() => {}}
-      />
+      <PromptCardList data={filteredPosts} handleTagClick={() => {}} />
     </section>
   );
 };
